f290fdee5a22b45f0dfb5fba0b9944f34e88d465
Alec Davis <unlikelytitan@gmail.com>
2024-04-11 11:35:17 -0600

    added fingerprint reader support to x230t

--- hosts/think-nix-x230t/configuration.nix
+++ hosts/think-nix-x230t/configuration.nix
@@ -2,6 +2,25 @@
 {
     hardware.opentabletdriver.enable = true;
     hardware.opentabletdriver.daemon.enable = true;
-    
+    services.fprintd = {
+        enable = true;
+    };
+
+    environment.systemPackages = with pkgs; [
+        fprintd
+    ];
+
+    /*security.pam.services.sudo.text = ''
+        account required pam_unix.so
+
+        auth sufficient pam_unix.so likeauth try_first_pass nullok
+        auth sufficient ${fprintd-clients}/lib/security/pam_fprintd.so
+        auth required pam_deny.so
+
+        password sufficient pam_unix.so nullok sha512
+        
+        session required pam_env.so conffile=/etc/pam/environment readenv=0
+        session required pam_unix.so
+    '';*/
 }
 
