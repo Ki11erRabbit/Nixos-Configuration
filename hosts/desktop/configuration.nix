a5152382a37f536572e58a680ad669e7f7727d27
ki11errabbit <ki11errabbit@posteo.net>
2024-07-04 13:17:31 -0700

    added dns servers

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -4,6 +4,10 @@
 
     networking.hostName = "nix-desktop"; # Define your hostname.
 
+    networking = {
+        nameservers = [ "192.168.254.12" "192.168.254.13" "192.168.0.121" ];
+        };
+
     # Configure keymap in X11
     services.xserver = {
         xkb.layout = "us";
