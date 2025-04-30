a0818827dcd536191f0d0bf3ae8a91db02cecbcd
ki11errabbit <ki11errabbit@posteo.net>
2025-04-30 15:43:57 -0700

    fixed wallpaper and lockscreen scripts to work with just bash

--- home/desktop-home.nix
+++ home/desktop-home.nix
@@ -66,13 +66,15 @@
             # $2 wallpaper list
             # $3 wallpaper dir
             function select_wallpaper () {
-                if [[ $(stat --printf="%s" $2) == "0" ]]; then
-                    find "$3" -name "*.png" -or -name "*.jpeg" -or -name "*.jpg" -or -name "*.webm" -or -name "*.mp4" | shuf > $2
+                if [[ $(stat --printf="%s" "$2") == "0" ]]; then
+                    find "$3" -name "*.png" -or -name "*.jpeg" -or -name "*.jpg" -or -name "*.webm" -or -name "*.mp4" | shuf > "$2"
                     file=$(cat $2 | head -n 1)
                 else 
                     file=$(cat $2 | head -n 1)
                 fi
-                tail -n +2 $2 > $2
+                exec 19<> "$2"
+                tail -n +2 "$2" >&19 
+                exec 19>&-
                 echo "$1: $file" >> $HOME/wallpaper.log
                 echo "$file"
 
@@ -120,22 +122,24 @@
             # $3 wallpaper dir
             # $4 display
             function select_wallpaper () {
-                if [[ $(stat --printf="%s" $2) == "0" ]]; then
-                    find "$3" -name "*.png" -or -name "*.jpeg" -or -name "*.jpg" -or -name "*.webm" -or -name "*.mp4" | shuf > $2
+                if [[ $(stat --printf="%s" "$2") == "0" ]]; then
+                    find "$3" -name "*.png" -or -name "*.jpeg" -or -name "*.jpg" -or -name "*.webm" -or -name "*.mp4" | shuf -o "$2"
                     file=$(cat $2 | head -n 1)
                 else 
                     file=$(cat $2 | head -n 1)
                 fi
-                tail -n +2 $2 > $2
+                exec 19<> "$2"
+                tail -n +2 "$2" >&19 
+                exec 19>&-
                 echo "$1: $file" >> $HOME/lockscreen.log
                 echo "$4:$file"
 
             }
 
 
-            file1=$(select_wallpaper "wide1" $widepapers_list $widepapers DP-1)
-            file2=$(select_wallpaper "wide2" $widepapers_list $widepapers DP-2)
-            file3=$(select_wallpaper "tall" $tallpapers_list $tallpapers HDMI-A-1)
+            file1=$(select_wallpaper "wide1" "$widepapers_list" "$widepapers" DP-1)
+            file2=$(select_wallpaper "wide2" "$widepapers_list" "$widepapers" DP-2)
+            file3=$(select_wallpaper "tall" "$tallpapers_list" "$tallpapers" HDMI-A-1)
 
             swaylock -ef -i "$file1" -i "$file2" -i "$file3"
             '';
