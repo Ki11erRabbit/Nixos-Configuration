b50263365c3339532e42795222a240a1add886d7
Alec Davis <unlikelytitan@gmail.com>
2025-09-19 17:05:02 -0600

    added lib to macos-home

--- home/macos-home.nix
+++ home/macos-home.nix
@@ -1,13 +1,17 @@
-{ config, pkgs, ... }:
+{ lib, config, pkgs, ... }:
 
 {
 
-    home.homeDirectory = "/home/ki11errabbit";
+    home.homeDirectory = "/Users/ki11errabbit";
     home.packages = with pkgs; [
         iterm2
         coqPackages.vscoq-language-server
     ];
     
+    home.activation.makeTrampolineApps = lib.hm.dag.entryAfter [ "writeBoundary" ] (
+        builtins.readFile ./make-app-trampolines.sh
+    );
+
     programs.zsh = {
         sessionVariables = {
         };
